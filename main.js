6a9d27fa26d3278db6f88b0b41665d57fc4b76a8
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-07-02 20:05:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Don't copy
+
+function terms() {
+	window.open("_terms/term.html");
+}
+
+function mail() {
+	window.open("mailto:nestcoderlive@gmail.com");
+}
+
+function imageOpen() {
+	window.open("img/events.png");
+}
